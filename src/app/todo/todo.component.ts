d530648238599a3ee11bb1a92c08b84be17db286
edidee <etokedidionge@gmail.com>
2021-08-02 20:45:43 +0100

    implemented remove and done

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -8,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class TodoComponent implements OnInit {
   value = '';
   completed: boolean = false;
-  onEnter(value: string) { this.value = value; }
-
+  selectedId: number = -1
+  taskList: any[] = []
+  addTask(value: string) {
+    this.taskList.push({ id: this.taskList.length, name: value })
+    console.log(this.taskList)
+  }
+  removeTask(id: number) {
+    
+    this.taskList= this.taskList.filter(value=>value.id!== id)
+    console.log(this.taskList);
+    
+  }
+  markDone(id: number) {
+  this.selectedId = id
+}
   constructor() { }
 
   ngOnInit(): void {
